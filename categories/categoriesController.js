305bb8a53ed9381dfafeee862f96de94288afc76
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-02-17 01:02:57 -0300

    Adicionando CSS e Navbar

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-  res.send("ROTA DE CATEGORIAS");
+router.get("/admin/categories/new", (req, res)=>{
+res.render("admin/categories/new")
 });
-
-router.get("/admin/categories/new", (req, res) => {
-  res.send("ROTA PARA CRIAR UMA NOVA CATEGORIA");
-});
-
 
 module.exports = router;
