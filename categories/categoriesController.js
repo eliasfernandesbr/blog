215eb9e04ce49162b5937c3423f76657e8191ae0
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-02-17 15:16:32 -0300

    Criando cadastro de categoria na rota save

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const category = require("./category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res)=>{
 res.render("admin/categories/new")
 });
+
+router.post("/categories/save", (req, res)=>{
+    var title = req.body.title;
+    if(title != undefined){
+        category.create({
+            title: title,
+            slug: slugify(title),
+        }).then(()=>{
+            res.redirect("/");
+        })
+
+    }else{
+        res.redirect("/admin/categories/new")
+    }
+});
+
 
 module.exports = router;
