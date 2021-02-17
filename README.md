66ad9462d878e6b8e6243a2910de274cd098368e
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-02-16 22:10:42 -0300

    Adicionando controllers de categoria e artigos + router

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Blog pessoal do Elias Fernandes
+   "start": "nodemon index.js -e ejs,js,css,html,jpg,png,scss",
--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const bodyParser = require ("body-parser")
-const connection = require("./database/database")
-
-
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const reaload = require("reload");
+const categController = require("./categories/categoriesController")
+const articController = require("./articles/articlesController")
 
 //VIEW ENGINE
 app.set("view engine", "ejs");
 
-
 // ARQUIVOS ESTATICOS- CSS, IMGS, JS do Front etc
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 //BODY-PARSER - TORNANDO POSSÍVEL TRABALHAR COM FORMULÁRIOS
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //DATABASE
 connection
-    .authenticate()
-    .then(()=>{
-        console.log("CONECTADO AO BANCO DE DADOS");
-    }).catch((error) =>{
-        console.log(error);
-    })
+  .authenticate()
+  .then(() => {
+    console.log("CONECTADO AO BANCO DE DADOS");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use("/", categController )
+app.use("/", articController )
 
 
 
-app.get("/", (req, res) =>{
-    res.render("index")
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.listen(3000, ()=>{
-    console.log("MEU SERVIDOR ESTÁ RODANDO");
-})
+app.listen(3000, () => {
+  console.log("MEU SERVIDOR ESTÁ RODANDO NA PORTA 3000");
+});
+reaload(app);
