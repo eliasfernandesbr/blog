252e40406ac3641c11f9f74a1920eb2d2a5f53d4
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-02-18 00:40:11 -0300

    Função de deletar categoria

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -3,31 +3,53 @@ const router = express.Router();
 const category = require("./category");
 const slugify = require("slugify");
 
-router.get("/admin/categories/new", (req, res)=>{
-res.render("admin/categories/new")
+router.get("/admin/categories/new", (req, res) => {
+  res.render("admin/categories/new");
 });
 
-router.post("/categories/save", (req, res)=>{
-    var title = req.body.title;
-    if(title != undefined){
-        category.create({
-            title: title,
-            slug: slugify(title),
-        }).then(()=>{
-            res.redirect("/");
+router.post("/categories/save", (req, res) => {
+  var title = req.body.title;
+  if (title != undefined) {
+    category
+      .create({
+        title: title,
+        slug: slugify(title),
+      })
+      .then(() => {
+        res.redirect("/");
+      });
+  } else {
+    res.redirect("/admin/categories/new");
+  }
+});
+
+router.get("/admin/categories", (req, res) => {
+  category.findAll().then((categories) => {
+    res.render("admin/categories/index", { categories: categories });
+  });
+});
+
+router.post("/categories/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      category
+        .destroy({
+          where: {
+            id: id,
+          },
         })
-
-    }else{
-        res.redirect("/admin/categories/new")
+        .then(() => {
+          res.redirect("/admin/categories");
+        });
+    } else {
+      //NÃO FOR NUMERO
+      res.redirect("/admin/categories");
     }
+  } else {
+    //FOR NULL
+    res.redirect("/admin/categories");
+  }
 });
-
-router.get("/admin/categories", (req, res)=>{
-    category.findAll().then(categories =>{
-        res.render("admin/categories/index", {categories: categories});
-    });
-    
-});
-
 
 module.exports = router;
